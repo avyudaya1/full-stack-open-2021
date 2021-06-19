547cee86da111215313171a2ce3d381ea351f0b5
avyudaya1 <avyudaya1@gmail.com>
2021-06-19 09:36:40 +0545

    completed 6.b before exercise 6.9

--- practice/redux-notes/src/App.js
+++ practice/redux-notes/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import NewNote from './components/NewNote'
+import VisibilityFiler from './components/VisibilityFilter'
 import Notes from './components/Notes'
 
 const App = () => {
   return (
     <div>
       <NewNote/>
+      <VisibilityFiler/>
       <Notes/>
     </div>
   )
